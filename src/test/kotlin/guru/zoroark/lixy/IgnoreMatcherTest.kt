bc2037ee861d8f032f9aa0f0bb83f75f601f5036
utybo <utybodev@gmail.com>
2020-02-02 21:15:25 +0200

    Add a test on multiple states for ignoring

--- src/test/kotlin/guru/zoroark/lixy/IgnoreMatcherTest.kt
+++ src/test/kotlin/guru/zoroark/lixy/IgnoreMatcherTest.kt
@@ -28,5 +28,37 @@ class IgnoreMatcherTest {
         val result = lexer.tokenize(string)
         assertEquals(expected, result)
     }
+
+    @Test
+    fun `Ignore matcher on string in multiple states`() {
+        val tkey = tokenType()
+        val tvalue = tokenType()
+        val svalue = stateLabel()
+        val lexer = lixy {
+            default state {
+                matches("[a-zA-Z_ ]+") isToken tkey
+                "->".ignore thenState svalue
+            }
+            svalue state {
+                matches("[a-zA-Z_ ]+") isToken tvalue
+                "\n".ignore thenState default
+            }
+        }
+        val string = """
+            key->value
+            one->two
+            Example_Yes->B e e_
+        """.trimIndent()
+        val expected = listOf(
+            LixyToken("key", 0, 3, tkey),
+            LixyToken("value", 5, 10, tvalue),
+            LixyToken("one", 11, 14, tkey),
+            LixyToken("two", 16, 19, tvalue),
+            LixyToken("Example_Yes", 20, 31, tkey),
+            LixyToken("B e e_", 33, 39, tvalue)
+        )
+        val result = lexer.tokenize(string)
+        assertEquals(expected, result)
+    }
 }
 
