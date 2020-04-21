ebf3bb72f9624842b10635d5e399427d5151bbfc
utybo <utybodev@gmail.com>
2020-04-21 14:33:28 +0200

    Add name property in LixyGenericTokenType (fixes #5)

--- src/main/kotlin/guru/zoroark/lixy/LixyTokenType.kt
+++ src/main/kotlin/guru/zoroark/lixy/LixyTokenType.kt
@@ -9,13 +9,16 @@ interface LixyTokenType
  * A generic class for token types. [tokenType] returns tokens of this type.
  * Big lexers should be using an enum which implements [LixyTokenType] instead
  * of [tokenType] and [LixyGenericTokenType].
+ *
+ * @property name A name for this token type, useful for debugging.
  */
-class LixyGenericTokenType :
-    LixyTokenType
+data class LixyGenericTokenType(val name: String) : LixyTokenType
 
 /**
  * Creates a new, distinct token type and returns it. The returned token type is
  * of the type [LixyGenericTokenType]
+ *
+ * @param name An optional name for the token type
  */
-fun tokenType(): LixyGenericTokenType =
-    LixyGenericTokenType()
+fun tokenType(name: String = ""): LixyGenericTokenType =
+    LixyGenericTokenType(name)
